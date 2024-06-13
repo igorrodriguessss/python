3ad985a5d9ffe5f9c355195a09033f2c27f69cb6
igorrodriguessss <igorgui3103@gmail.com>
2024-06-13 13:27:43 -0300

    atualizado

--- musico.py
+++
@@ -1,6 +0,0 @@
-class Musico:
-    def __init__(self, nome, instrumento):
-       self.nome = nome
-       self.instrumento = instrumento
-    def mostrar_info(self):
-       print(f'Músico: {self.nome}, Instrumento: {self.instrumento}') 
